98ba84a15ec5d4633d39de6375b381292b296a93
anggakusumap <kusuma_putra@student.unud.ac.id>
2024-03-13 11:13:28 +0800

    feat: nav mobile

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div className="bg-cover min-h-screen px-8 py-24 flex flex-col justify-between" style={ { backgroundImage: "url(/img/hero.png)" } }>
-            <div className="text-center text-third flex flex-col gap-10">
-                <div className="max-w-6xl 2xl:max-w-7xl pt-20">
-                    <h1 className="text-5xl md:text-[10rem] 2xl:text-[12rem] font-bold text-end">360<sup>o</sup> REALTY</h1>
-                    <p className="text-md md:text-3xl text-end">Property sales and management solutions</p>
+        <div className="bg-cover px-8 py-8 min-h-screen flex flex-col justify-between" style={ { backgroundImage: "url(/img/hero.png)" } }>
+            <div className="text-center text-third flex flex-col gap-10 md:pt-20">
+                <div className="max-w-6xl 2xl:max-w-7xl pt-20 2xl:pt-40 text-center md:text-end">
+                    <h1 className="text-5xl md:text-[10rem] 2xl:text-[12rem] font-bold">360<sup>o</sup> REALTY</h1>
+                    <p className="text-md md:text-3xl">Property sales and management solutions</p>
                 </div>
             </div>
             <div className="flex flex-col items-start gap-4">
-                <div className="rounded-lg text-center w-96 bg-base-100 shadow-xl shadow-sky-400 p-4">
-                    <div className="flex flex-col gap-2">
-                        <h2 className="text-4xl font-bold">12 Months FREE</h2>
-                        <p className="text-xl">Villa Management Services</p>
-                        <button className="btn bg-primary text-white rounded-full w-auto mx-auto">Learn More</button>
+                <div className="rounded-lg text-center w-96 max-w-full bg-base-100 bg-white shadow-xl shadow-sky-400 p-4">
+                    <div className="flex flex-col gap-4">
+                        <div className="flex flex-col">
+                            <h2 className="text-4xl font-bold">12 Months FREE</h2>
+                            <p className="text-xl">Villa Management Services</p>
+                        </div>
+                        <button className="p-2 bg-primary text-white rounded-full w-auto mx-auto">Learn More</button>
                         <p className="uppercase text-xs"><span className="font-bold">Limited time only - </span>TERMS & CONDITIONS APPLY</p>
                     </div>
                 </div>
--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,50 +1,50 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+    const [ showMobile, setShowMobile ] = useState(false);
+
     return (
-        <header>
-            <nav className="bg-[#D9D9D9]/30">
-                <div className="mx-auto max-w-full px-8">
-                    <div className="relative flex h-16 items-center justify-between">
-                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                                <span className="absolute -inset-0.5"></span>
-                                <span className="sr-only">Open main menu</span>
-                                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                                </svg>
-                                <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
-                        </div>
+        <header className="fixed top-0 w-full bg-[#F4F4F4] z-10">
+            <nav>
+                <div className="mx-auto max-w-full px-2 md:px-8 py-3">
+                    <div className="flex items-center justify-between">
+                        { showMobile ?
+                            <IoClose onClick={ () => setShowMobile(false) } color='black' className="md:hidden text-xl" /> :
+                            <FaBars onClick={ () => setShowMobile(true) } color='black' className="md:hidden text-xl" />
+                        }
                         <div>
                             <h1 className="font-semibold text-2xl">360<sup>o</sup> REALTY</h1>
                             <p className="text-xs">Sales & Management Solutions</p>
                         </div>
-                        <div className="flex flex-1 items-center justify-end">
-                            <div className="hidden sm:ml-6 sm:block">
-                                <div className="flex space-x-10 justify-end">
-                                    <a href="#" className="text-third uppercase text-sm font-semibold" aria-current="page">Home</a>
-                                    <a href="#" className="text-primary uppercase text-sm font-medium">Contact</a>
-                                    <a href="#" className="text-primary uppercase text-sm font-medium">Services</a>
-                                    <a href="#" className="text-primary uppercase text-sm font-medium">Catalog</a>
-                                    <a href="#" className="text-primary uppercase text-sm font-medium">Pricing</a>
-                                    <a href="#" className="text-primary uppercase text-sm font-medium">About Us</a>
-                                    <a href="#" className="text-third uppercase text-sm font-medium">Eng</a>
-                                </div>
+                        <div className="hidden md:flex flex-1 items-center justify-end">
+                            <div className="flex space-x-10 justify-end">
+                                <a href="#" className="text-third uppercase text-sm font-semibold" aria-current="page">Home</a>
+                                <a href="#" className="text-primary uppercase text-sm font-medium">Contact</a>
+                                <a href="#" className="text-primary uppercase text-sm font-medium">Services</a>
+                                <a href="#" className="text-primary uppercase text-sm font-medium">Catalog</a>
+                                <a href="#" className="text-primary uppercase text-sm font-medium">Pricing</a>
+                                <a href="#" className="text-primary uppercase text-sm font-medium">About Us</a>
+                                <a href="#" className="text-third uppercase text-sm font-medium">Eng</a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="sm:hidden" id="mobile-menu">
-                    <div className="space-y-1 px-2 pb-3 pt-2">
-                        <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+                { showMobile &&
+                    <div className="sm:hidden" id="mobile-menu">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Services</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Catalog</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Pricing</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About Us</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Eng</a>
+                        </div>
                     </div>
-                </div>
+                }
             </nav>
         </header>
     );
